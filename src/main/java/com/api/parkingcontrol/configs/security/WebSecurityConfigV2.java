d86177311d9cbf46d65710aa94d390f01acd1616
andre06x <andre06x@gmail.com>
2022-11-16 16:48:25 -0300

    security

---
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfigV2.java
@@ -0,0 +1,57 @@
+package com.api.parkingcontrol.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigV2 {
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+//                .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/parking-spot/**").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
